3dc23d6892939433c5f59b89ecb4d16db2e9587c
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-01-27 19:20:55 +0100

    Adds some example products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create!(title: 'Ruby on Rails Poster',
+                description:
+                    %{<p>
+      <em>Imagine what you could build if you learned Ruby on Rails…</em>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+      sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>},
+                image_url: 'product_rails.jpg',
+                price: 10.00)
+# . . .
+Product.create!(title: 'Ruby Poster',
+                description:
+                    %{<p>
+      <em>A dynamic, open source programming language with a focus on simplicity and productivity./em>
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+      tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+      vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+      no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>},
+                image_url: 'product_ruby.jpg',
+                price: 8.00)
+# . . .
+
+Product.create!(title: 'Node.js Poster',
+                description:
+                    %{<p>
+      <em>Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient.</em>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+      Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+      Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      </p>},
+                image_url: 'product_node_js.jpg',
+                price: 12.00)
